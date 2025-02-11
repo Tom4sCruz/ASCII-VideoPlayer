7007230e4c15e719a967e56fcd2c2bdfd510104f
Tom4sCruz <tomascruz2300@gmail.com>
2025-02-11 15:58:49 +0000

    Image-to-ASCII and Video-to-ASCII scripts done. no FPS cap on Video-to-ASCII. Added an image and a video as defaults

---
+++ image_to_ascii.py
@@ -0,0 +1,35 @@
+from PIL import Image
+
+CHARS = ' .;+*?%S#@'
+
+AM = 2 # Arbitrary Multiplier
+
+def image_to_ascii(path):
+    with Image.open(path) as img:
+        # Converts image to Grayscale
+        img = img.convert("L")
+        
+        width,height = img.size
+        new_h = 148
+        new_w = AM * int(new_h / height * width)
+        # Resizes image
+        img = img.resize((new_w, new_h))
+        
+        pixels = img.getdata()
+        char_pixels = [CHARS[pixel//26] for pixel in pixels]
+        
+        num_pixels = len(char_pixels)
+
+        ascii_image = [''.join(char_pixels[i:i+new_w]) for i in range(0, num_pixels, new_w)]
+        ascii_image = '\n'.join(ascii_image)
+
+        print(ascii_image)
+
+        with open(f"$HOME/Pictures/ASCII/{path.split('/')[-1].split('.')[0]}.txt", "w") as f:
+            f.write(ascii_image)
+
+if __name__ == "__main__":
+    path = input("Paste the Path for an Image: ")
+    path = "black_hole.jpg" if path.strip() == '' else path
+    image_to_ascii(path)
+
---
+++ video_to_ascii.py
@@ -0,0 +1,37 @@
+import time, os, cv2
+
+CHARS = ' .;+*?%S#@'
+
+AM = 6 # Arbitrary Multiplier
+
+def frame_to_ascii(image):
+    width = image.shape[0]
+    height = image.shape[1]
+    new_h = 148
+    new_w = int(new_h/height * width)
+    image = cv2.resize(image, (AM*new_w, new_h))
+    
+    grayscale = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    
+    ascii_frame = '\n'.join([''.join([CHARS[pixel//26] for pixel in row]) for row in grayscale])
+    return ascii_frame
+
+
+def video_to_ascii(path):
+    cap = cv2.VideoCapture(path)
+    if not cap.isOpened():
+        print("\033[91mCannot open video file. Try again.\033[0m")
+    
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            #print("\033[91mCannot receive frame. Exiting...\033[0m")
+            break
+        
+        ascii_frame = frame_to_ascii(frame)
+        print(ascii_frame, end="\r") # Will need to add 'clear' instead
+
+if __name__ == "__main__":
+    path = input("Paste the path to your video: ")
+    path = "Meta_PARTNR.webm" if path.strip() == '' else path
+    video_to_ascii(path)
